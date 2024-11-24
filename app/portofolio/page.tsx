c048c31d5fc54edc82a9d5fda93640d4638b37ed
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2024-11-24 13:00:45 +0700

    fix grid cols

--- app/portofolio/page.tsx
+++ app/portofolio/page.tsx
@@ -53,7 +53,7 @@ export default function PortofolioPage() {
                 ></SearchBox>
               </div>
               <div>
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                   <InfiniteHits
                     hitComponent={Hit}
                     showPrevious={false}
