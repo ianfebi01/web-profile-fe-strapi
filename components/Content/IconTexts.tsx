5a8ddbd0e2cdf79b6788b8c41f2007648ae4c315
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2025-01-18 23:21:15 +0700

    fix: disable link

--- components/Content/IconTexts.tsx
+++ components/Content/IconTexts.tsx
@@ -25,13 +25,15 @@ const Gallery: React.FC<Props> = ( { sectionData } ) => {
         <Link
           key={index}
           className={cn(
-            'flex flex-col items-center p-4 gap-4 basis-full md:basis-[calc(100%/2-1.5rem)] lg:basis-[calc(100%/3-1.5rem)] !text-inherit no-underline cursor-default',
+            'flex flex-col items-center p-4 gap-4 basis-full md:basis-[calc(100%/2-1.5rem)] lg:basis-[calc(100%/3-1.5rem)] !text-inherit no-underline cursor-default pointer-events-none',
             item?.link &&
-              'hover:shadow-xl cursor-pointer transition-all duration-300 ease-in-out'
+              'hover:shadow-xl cursor-pointer transition-all duration-300 ease-in-out pointer-events-auto'
           )}
           href={item?.link || ''}
           target={item?.linkNewTab ? '_blank' : undefined}
           rel={item?.linkNewTab ? 'noopener noreferrer' : undefined}
+          aria-disabled={!item?.link} 
+          tabIndex={!item?.link ? -1 : undefined}
         >
           <div className="aspect-square w-full overflow-hidden relative">
             {item.image?.data && (
