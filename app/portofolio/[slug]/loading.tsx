9af093b8acb4c63375a07272c03d1173a75fb20e
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2025-01-27 05:32:57 +0700

    add skeleton

---
+++ app/portofolio/[slug]/loading.tsx
@@ -0,0 +1,5 @@
+import SkeletonDetail from "@/components/Pages/Portofolio/SkeletonDetail";
+
+export default function Loading() {
+  return <SkeletonDetail />
+}
--- components/Pages/Portofolio/SkeletonDetail.tsx
+++ components/Pages/Portofolio/SkeletonDetail.tsx
@@ -1,6 +1,6 @@
 const SkeletonDetail = () => {
   return (
-    <div className="article__container mt-20 sm:mt-20 mb-8 flex flex-col gap-4 animate-pulse">
+    <div className="max-w-5xl mx-auto w-full mt-20 sm:mt-20 mb-8 flex flex-col gap-4 animate-pulse">
       <div className="my-4 flex flex-col gap-2">
         <div className="h-12 min-w-60 bg-dark-secondary"></div>
         <div className="h-4 max-w-xs bg-dark-secondary"></div>
