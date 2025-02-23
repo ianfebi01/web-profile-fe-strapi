291e2863cd2e6f6eea2d7018cb9464fac045686b
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2025-02-23 19:12:11 +0700

    chore: reset to 34cf2d2

--- components/Parsers/Markdown.tsx
+++ components/Parsers/Markdown.tsx
@@ -2,8 +2,9 @@
 import parseMd from '@/utils/parseMd'
 import sanitize from '@/utils/sanitize'
 import truncate from '@/utils/truncate'
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import 'highlight.js/styles/atom-one-dark.css'
+import hljs from 'highlight.js'
 
 interface Props {
   content: string
@@ -12,49 +13,34 @@ interface Props {
 
 const Markdown = ( { content, excerpt }: Props ) => {
   const bodyCopyRef = useRef<HTMLDivElement>( null )
+  const [parsedContent, setParsedContent] = useState<string>( '' )
 
   useEffect( () => {
+    // Parse and sanitize the content
+    const parsed = sanitize( parseMd( content ), 'richtext' )
+    setParsedContent( excerpt ? truncate( parsed, excerpt ) : parsed )
+  }, [content, excerpt] )
+
+  useEffect( () => {
+    if ( !bodyCopyRef.current ) return
+
     // Add target and rel attributes to all <a> tags
-    const aTags = bodyCopyRef.current?.querySelectorAll( 'a' )
-    if ( aTags?.length ) {
-      aTags.forEach( ( t ) => {
-        t?.setAttribute( 'target', '_blank' )
-        t?.setAttribute( 'rel', 'noopener noreferrer' )
-      } )
-    }
+    const aTags = bodyCopyRef.current.querySelectorAll( 'a' )
+    aTags.forEach( t => {
+      t.setAttribute( 'target', '_blank' )
+      t.setAttribute( 'rel', 'noopener noreferrer' )
+    } )
 
-    // Apply syntax highlighting to all <pre><code> blocks
-    // hljs.highlightAll();
-    const loadHljs = async () => {
-      const hljs = ( await import( 'highlight.js' ) ).default
-      if ( bodyCopyRef.current ) {
-        bodyCopyRef.current.querySelectorAll( 'pre code' ).forEach( ( block ) => {
-          hljs.highlightElement( block as HTMLElement )
-        } )
-      }
-    }
-
-    loadHljs()
-  }, [content] )
+    // Apply syntax highlighting
+    hljs.highlightAll()
+  }, [parsedContent] ) // Ensure highlighting runs only after parsing is complete
 
   return (
     <div ref={bodyCopyRef}
       className="body-copy w-full"
     >
-      {!excerpt && !!content ? (
-        <div
-          dangerouslySetInnerHTML={{
-            __html : sanitize( parseMd( content ), 'richtext' ),
-          }}
-        ></div>
-      ) : !!content ? (
-        <div
-          dangerouslySetInnerHTML={{
-            __html : truncate( sanitize( parseMd( content ), 'richtext' ), excerpt ),
-          }}
-        ></div>
-      ) : (
-        ''
+      {parsedContent && (
+        <div dangerouslySetInnerHTML={{ __html : parsedContent }}></div>
       )}
     </div>
   )
