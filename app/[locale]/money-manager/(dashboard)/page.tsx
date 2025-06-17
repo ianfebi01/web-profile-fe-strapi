e6f81bb59058038d8be578aff0b39c47470b54ae
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2025-06-17 20:31:39 +0700

    FEAT: dashboard page

--- app/[locale]/money-manager/(dashboard)/page.tsx
+++ app/[locale]/money-manager/(dashboard)/page.tsx
@@ -1,3 +1,6 @@
+
+import Header from '@/components/Layouts/Header'
+import Dashboard from '@/components/Pages/Dashboard'
 import { Locale } from 'next-intl'
 import { getTranslations } from 'next-intl/server'
 
@@ -13,5 +16,10 @@ export default async function PageMoneyManager( { params }: Props ) {
 
   const t = await getTranslations( { locale } )
 
-  return <div>{t( 'cashflow' )}</div>
+  return (
+    <div className='mt-8'>
+      <Header text={t( 'summary' )} />
+      <Dashboard/>
+    </div>
+  )
 }
