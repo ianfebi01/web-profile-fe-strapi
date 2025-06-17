5999f6e84a64c7c329ee3cab980bde85f2747e44
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2025-06-17 20:32:23 +0700

    FEAT: add curency

--- utils/format-curency.ts
+++ utils/format-curency.ts
@@ -1,4 +1,19 @@
-const formatCurency = ( value: number ) => {
+const formatCurency = ( value: number, currency = 'IDR' ) => {
+  if ( currency === 'IDR' ) {
+    return value.toLocaleString( 'id-ID', {
+      style    : 'currency',
+      currency : 'IDR',
+    } )
+  }
+  if ( currency === 'IDRK' ) {
+    const formatted = ( value / 1000 ).toLocaleString( 'id-ID', {
+      minimumFractionDigits : 0,
+      maximumFractionDigits : 1,
+    } )
+
+    return `Rp. ${formatted}K`
+  }
+
   return value.toLocaleString( 'id-ID', {
     style    : 'currency',
     currency : 'IDR',
