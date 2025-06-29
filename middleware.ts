899ee2fe1bf6caa793ad3c24fc3d6b77bc464ac8
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2025-06-29 14:12:48 +0700

    CHORE: add matcher to exclude sitemap and robot

--- middleware.ts
+++ middleware.ts
@@ -12,5 +12,5 @@ export default function middleware( req: NextRequest ) {
 }
 
 export const config = {
-  matcher : '/((?!api|trpc|_next|_vercel|.*\\..*).*)',
+  matcher : '/((?!api|trpc|_next|_vercel|.*\\..*|sitemap\\.xml|robots\\.txt).*)',
 }
