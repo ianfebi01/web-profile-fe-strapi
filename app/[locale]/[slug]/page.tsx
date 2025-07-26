20121eab113056ff59b98ff9d05efaf6c00dfb03
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2025-07-27 01:25:14 +0700

    FEAT: add metadata on pages

--- app/[locale]/[slug]/page.tsx
+++ app/[locale]/[slug]/page.tsx
@@ -3,6 +3,9 @@ import HeroesAndSections from '@/components/Parsers/HeroesAndSections'
 import { notFound } from 'next/navigation'
 import { ApiPagePage } from '@/types/generated/contentTypes'
 import { getAllPageSlugs } from '@/lib/api/pagesQueryFn'
+import imageUrl from '@/utils/imageUrl'
+import { FALLBACK_SEO } from '@/utils/constants'
+import { Metadata } from 'next'
 
 type Props = {
   params: {
@@ -17,9 +20,60 @@ export async function generateStaticParams() {
   return (
     slugs?.map( ( slug: ApiPagePage ) => ( {
       slug   : slug.attributes.slug,
-      locale : slug.attributes.locale
+      locale : slug.attributes.locale,
     } ) ) || []
   )
+}
+
+export async function generateMetadata( { params }: Props ): Promise<Metadata> {
+  const page = await getPageBySlug( params.slug, params.locale )
+
+  const metadata = page.data[0]?.attributes?.seo
+
+  // Extract social metadata
+  const socialMeta = metadata?.metaSocial?.length
+    ? Object.fromEntries(
+      metadata?.metaSocial?.map(
+        ( { socialNetwork, title, description, image }: any ) => [
+          socialNetwork.toLowerCase(),
+          { title, description, image },
+        ]
+      )
+    )
+    : {}
+
+  // Extract image data for Open Graph and Twitter
+
+  return {
+    title :
+      metadata?.metaTitle ||
+      page?.data[0]?.attributes?.title ||
+      FALLBACK_SEO.title ||
+      null,
+    description : metadata?.metaDescription || FALLBACK_SEO.description || null,
+    keywords    : metadata?.keywords || null,
+    robots      : metadata?.metaRobots || null,
+    openGraph   : {
+      url         : metadata?.canonicalURL || null,
+      title       : metadata?.metaTitle || null,
+      description : metadata?.metaDescription || null,
+      siteName    : 'Ian Febi Sastrataruna', // Replace with your site name
+      type        : 'website', // or "article"
+      images      : metadata?.metaImage?.data
+        ? [{ url : imageUrl( metadata?.metaImage?.data, 'medium' ) || '' }]
+        : [], // Add Open Graph image
+    },
+    twitter : {
+      card  : 'summary',
+      site  : '@ianfebi01',
+      title : metadata?.metaTitle || FALLBACK_SEO.title || null,
+      description :
+        socialMeta?.twitter?.description || FALLBACK_SEO.description || '',
+      images : socialMeta?.twitter?.image?.data
+        ? [{ url : imageUrl( socialMeta?.twitter?.image?.data, 'medium' ) || '' }]
+        : [], // Twitter image
+    },
+  }
 }
 
 export default async function PageRoute( { params }: Props ) {
